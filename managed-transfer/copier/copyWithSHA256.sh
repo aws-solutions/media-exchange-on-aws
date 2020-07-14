7df8f04ebf61671e0dfc5de57f897d50dbbc982d
Provanshu Dey <prodey@amazon.com>
2020-07-13 23:41:37 -0700

    managed transfer + checksum

--- managed-transfer/copier/copyWithSHA256.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-[[ -z "$1" ]] && { echo "Error: s3://<Source> is required"; exit 1; }
-[[ -z "$2" ]] && { echo "Error: s3://<Destination> is required"; exit 1; }
-[[ -z "$3" ]] && { echo "Error: KMS Key Arn is required"; exit 1; }
-[[ -z "$4" ]] && { echo "Error: S3 Cannoical Id is required"; exit 1; }
-
-
-aws s3 cp $1 - | sha256sum | cut -d ' ' -f1 | xargs -I {} aws s3 cp $1 $2 --copy-props metadata-directive --metadata-directive REPLACE --metadata Content-SHA256={} --sse aws:kms --sse-kms-key-id $3 --grants read=id=$4
--- media-exchange-base/tools/push-content.sh
+++ media-exchange-base/tools/push-content.sh
@@ -1,4 +1,20 @@
-#! /bin/bash -x
+#! /bin/bash
+
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 ## set access keys, profile etc. -
 
@@ -26,7 +42,7 @@ CHECKSUM=$(openssl dgst -sha256 /tmp/$FILE_NAME | cut -d' ' -f2)
 echo checksum is $CHECKSUM
 
 echo copying to s3
-aws s3api put-object --bucket $BUCKET_NAME --key $FILE_NAME  --body /tmp/$FILE_NAME   --server-side-encryption aws:kms --ssekms-key-id $KMS_KEY_ID --grant-read id=$SUBSCRIBER_CANONICAL_ACCOUNT_ID
+aws s3api put-object --bucket $BUCKET_NAME --key $FILE_NAME  --body /tmp/$FILE_NAME  --server-side-encryption aws:kms --ssekms-key-id $KMS_KEY_ID --grant-read id=$SUBSCRIBER_CANONICAL_ACCOUNT_ID
 
 rm -rf /tmp/$FILE_NAME
 
@@ -34,7 +50,7 @@ echo getting list of objects
 aws s3api list-objects-v2 --bucket $BUCKET_NAME --fetch-owner
 
 aws events put-rule --name "XAccountEvents" --event-pattern "{\"source\":[\"$EVENT_SOURCE_NAME\"]}"
-aws events put-targets --rule "XAccountEvents" --targets "Id"="XAccountEvents","Arn"="arn:aws:events:$AWS_REGION:$MEDIAEXCHNAGE_ACCOUNTID:event-bus/default"
+aws events put-targets --rule "XAccountEvents" --targets "Id"="XAccountEvents","Arn"="arn:aws:events:$AWS_REGION:$MEDIAEXCHANGE_ACCOUNTID:event-bus/default"
 
 echo sending notification
 
