c446661e24e2991c047f0bc1f9112b6e00a0ee8e
Leah Siddall <lsiddall@amazon.com>
2020-10-27 15:03:25 -0700

    no code to unit test

--- deployment/run-unit-tests.sh
+++ deployment/run-unit-tests.sh
@@ -8,34 +8,38 @@
 #
 
 # Get reference for all important folders
-template_dir="$PWD"
-source_dir="$template_dir/../source"
+# template_dir="$PWD"
+# source_dir="$template_dir/../source"
+
+# echo "------------------------------------------------------------------------------"
+# echo "[Init] Clean old dist and node_modules folders"
+# echo "------------------------------------------------------------------------------"
+# echo "find $source_dir/services -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
+# find $source_dir/services -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
+# echo "find $source_dir/services -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
+# find $source_dir/services -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
+# echo "find ../ -type f -name 'package-lock.json' -delete"
+# find $source_dir/services -type f -name 'package-lock.json' -delete
+# echo "find $source_dir/resources -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
+# find $source_dir/resources -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
+# echo "find $source_dir/resources -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
+# find $source_dir/resources -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
+# echo "find ../ -type f -name 'package-lock.json' -delete"
+# find $source_dir/resources -type f -name 'package-lock.json' -delete
+# echo "find $source_dir/simulator -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
+# find $source_dir/simulator -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
+# echo "find $source_dir/simulator -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
+# find $source_dir/simulator -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
+# echo "find ../ -type f -name 'package-lock.json' -delete"
+# find $source_dir/simulator -type f -name 'package-lock.json' -delete
+
+# echo "------------------------------------------------------------------------------"
+# echo "[Test] Services - Example Function"
+# echo "------------------------------------------------------------------------------"
+# cd $source_dir/example-function-js
+# npm install
+# npm test
 
 echo "------------------------------------------------------------------------------"
-echo "[Init] Clean old dist and node_modules folders"
+echo "[INFO] Solutions has no code to unit test."
 echo "------------------------------------------------------------------------------"
-echo "find $source_dir/services -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/services -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find $source_dir/services -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/services -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find ../ -type f -name 'package-lock.json' -delete"
-find $source_dir/services -type f -name 'package-lock.json' -delete
-echo "find $source_dir/resources -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/resources -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find $source_dir/resources -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/resources -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find ../ -type f -name 'package-lock.json' -delete"
-find $source_dir/resources -type f -name 'package-lock.json' -delete
-echo "find $source_dir/simulator -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/simulator -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find $source_dir/simulator -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/simulator -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find ../ -type f -name 'package-lock.json' -delete"
-find $source_dir/simulator -type f -name 'package-lock.json' -delete
-
-echo "------------------------------------------------------------------------------"
-echo "[Test] Services - Example Function"
-echo "------------------------------------------------------------------------------"
-cd $source_dir/example-function-js
-npm install
-npm test
