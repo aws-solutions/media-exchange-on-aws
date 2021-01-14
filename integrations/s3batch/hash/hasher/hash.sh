318fc70beeaa0b212fd8ba894fb27605932db307
Provanshu Dey <prodey@amazon.com>
2021-01-14 12:26:07 -0800

    saving hash value to tags

--- integrations/s3batch/hash/hasher/hash.sh
+++ integrations/s3batch/hash/hasher/hash.sh
@@ -3,8 +3,14 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-[[ -z $1 ]] && { echo "Error: s3://<Source> is required"; exit 1; }
+# usage: ./hash.sh s3://<bucket>/<key>
+
+[[ -z $1 ]] && { echo "Error: s3://<bucket>/<key> is required"; exit 1; }
+
 
 aws s3 cp "$1" - | tee >(md5sum | cut -d ' ' -f1 > /tmp/MD5.result) >(sha1sum | cut -d ' ' -f1 > /tmp/SHA1.result) >(xxhsum | cut -d ' ' -f1 > /tmp/xxhsum.result) >(xxh64sum | cut -d ' ' -f1 > /tmp/xxh64sum.result) > /dev/null
 
-aws s3 cp "$1" "$1" --metadata-directive REPLACE --metadata Content-MD5=$(cat /tmp/MD5.result),Content-SHA1=$(cat /tmp/SHA1.result),Content-XXHash=$(cat /tmp/xxhsum.result),Content-XX64Hash=$(cat /tmp/xxh64sum.result)  --acl bucket-owner-full-control
+BUCKET=$(echo $1 | cut -f3 -d/)
+KEY=$(echo $1 | cut -f4 -d/)
+
+aws s3api put-object-tagging --bucket $BUCKET --key $KEY --tagging "TagSet=[{Key=Content-MD5,Value=$(cat /tmp/MD5.result)},{Key=Content-SHA1,Value=$(cat /tmp/SHA1.result)},{Key=Content-XXHash,Value=$(cat /tmp/xxhsum.result)},{Key=Content-XX64Hash,Value=$(cat /tmp/xxh64sum.result)}]"
